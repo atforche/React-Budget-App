dec098025656138874557c3350941d8899154734
Andrew <atforche@gmail.com>
2023-07-18 22:05:43 -0400

    Initial Commit

---
+++ CommandLine/CommandLineInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CommandLine;
+
+/// <summary>
+/// Helper class to manage command line input. Enables data to be imported
+/// from an Excel sheet into the SQL database or exported from the SQL database
+/// into an Excel sheet.
+/// </summary>
+public class CommandLineHelper
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        if (args.Length > 0)
+        {
+            Console.WriteLine(args[0]);
+        }
+    }
+}
